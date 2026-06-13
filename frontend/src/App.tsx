a2dde1b5f2e05113ddb070d8b8b99cf8f42b9016
andreeaamg99-lab <andreeaamg99@yahoo.com>
2026-06-13 15:31:46 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'; import { Clicker } from"./components/Clicker/Clicker"; import "./styles/global.scss"; import styles from"./App.module.scss"; import { ResourceCounter } from "./components/ResourceCounter"; import { MainObject } from "./components/MainObject"; import { Payment } from "./components/Payment";import { ProgressBar } from "./components/ProgressBar"; import { Upgrades } from"./components/Upgrades"; export const App = () => { const [coins, setCoins] = useState(0);const [crystals, setCrystals] = useState(0); const [progress, setProgress] = useState(0);return ( <div className={styles.container}> <ResourceCounter coins={coins} crystals={crystals} /> <ProgressBar progress={progress} /> <MainObject /> <Clicker setCoins={setCoins} setProgress={setProgress} /> <Upgrades coins={coins} setCoins={setCoins} /> <Payment /> </div> ); };
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { Clicker } from "./components/Clicker/Clicker";
 import "./styles/global.scss";
 import styles from "./App.module.scss";
